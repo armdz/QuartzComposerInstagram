8f0660502994b28fcc95faf1e99c419798c2217a
armdz <lolo@armdz.com>
2015-09-15 09:08:50 -0300

    Initial Commit

---
+++ ARMDZ_InstagramSearch/ARMDZ_InstagramSearchPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  ARMDZ_InstagramSearchPlugIn.h
+//  ARMDZ_InstagramSearch
+//
+//  Created by Lolo on 9/15/15.
+//  Copyright (c) 2015 ARMDZ. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface ARMDZ_InstagramSearchPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
